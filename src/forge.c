a3e83ef0b45535b600e4f7894f5988108f276c68
klemek <klemek.dev@proton.me>
2025-09-12 10:46:34 +0200

    fullscreen primary

--- src/forge.c
+++ src/forge.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "linmath.h"
 
@@ -66,8 +67,10 @@ void *init_window(GLFWwindow **window) {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_DECORATED, NULL);
 
-  (*window) = glfwCreateWindow(640, 480, PACKAGE " " VERSION, NULL, NULL);
+  (*window) = glfwCreateWindow(640, 480, PACKAGE " " VERSION,
+                               glfwGetPrimaryMonitor(), NULL);
 
   if (!(*window)) {
     fprintf(stderr, "[GLFW] Window or context creation failed\n");
