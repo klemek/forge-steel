1b6c78f0d478bd7ea7eb1ceb6bdb481e35556db1
klemek <klemek.dev@proton.me>
2025-11-11 10:24:24 +0100

    style: format python script

--- scripts/sentences.py
+++ scripts/sentences.py
@@ -2,7 +2,9 @@
 
 import os
 
-TARGET_FILE = os.path.join(os.path.dirname(__file__), "..", "default", "inc_sentences.glsl")
+TARGET_FILE = os.path.join(
+    os.path.dirname(__file__), "..", "default", "inc_sentences.glsl"
+)
 
 SENTENCES = [
     "forge",
@@ -24,14 +26,14 @@ SENTENCES = [
     "Art as code",
 ]
 
-MAPPING = {
-    "π": "0xE3"
-}
+MAPPING = {"π": "0xE3"}
+
 
 def code(char: str) -> str:
     if char in MAPPING:
         return MAPPING[char]
     return "0x" + f"{ord(char):02x}".upper()
+
 
 def convert(txt: str) -> str:
     out = []
@@ -39,19 +41,23 @@ def convert(txt: str) -> str:
         out += [code(txt[i]) if i < len(txt) else "0x00"]
     return f"{{{', '.join(out)}}}"
 
+
 def main() -> None:
-    with open(TARGET_FILE, mode = 'w') as file:
-        file.write(f"#ifndef INC_SENTENCES\n")
-        file.write(f"#define INC_SENTENCES\n\n")
+    with open(TARGET_FILE, mode="w") as file:
+        file.write("#ifndef INC_SENTENCES\n")
+        file.write("#define INC_SENTENCES\n\n")
         file.write(f"#define SENTENCE_COUNT {len(SENTENCES)}\n\n")
         file.write("const int sentences[SENTENCE_COUNT][20] = {\n")
         for sentence in SENTENCES:
             file.write("    " + convert(sentence) + "," + "\n")
         file.write("};\n\n")
         file.write("const int lengths[SENTENCE_COUNT] = {\n")
-        file.write("    " + ", ".join(str(len(sentence)) for sentence in SENTENCES) + "\n")
+        file.write(
+            "    " + ", ".join(str(len(sentence)) for sentence in SENTENCES) + "\n"
+        )
         file.write("};\n")
         file.write("#endif")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
