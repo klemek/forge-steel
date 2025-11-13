2ab094755088e88b1477c4ceaf66b121b3bcd950
klemek <klemek.dev@proton.me>
2025-11-13 19:32:38 +0100

    fix: don't black out screen in demo

--- default/frag8.glsl
+++ default/frag8.glsl
@@ -16,11 +16,14 @@ uniform sampler2D iTex0;
 uniform int iSeed8;
 uniform vec3 iMidi2_3[7];
 uniform vec3 iMidi3_1[2];
+uniform int iDemo;
 
 void main() {
     vec4 color = fx_stage(vUV, iTex9, iTex0, iSeed8, iMidi2_3[0], iMidi2_3[1].xy, iMidi2_3[2], iMidi2_3[3].xy, iMidi2_3[4], iMidi2_3[5].xy, iMidi2_3[6]);
 
-    color = mix(color, vec4(0), iMidi3_1[0].y);
+    if (iDemo < 1) {
+        color = mix(color, vec4(0), iMidi3_1[0].y);
+    }
 
     fragColor = color;
 }
